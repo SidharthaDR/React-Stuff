0bb0160bbc5912f286ffc31d016df8096282091b
Sidharth_DR <raghunathsidhartha5h5@gmail.com>
2024-12-04 18:44:30 +0530

    new-proj-on-pwdGenerator

--- tproject04_bgChanger/components/ButtonComp.jsx
+++ tproject04_bgChanger/components/ButtonComp.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 function ButtonComp({bbcolor,funck}) {
 
   return (
-    <div>
+    
         <button onClick={funck} className="p-1 px-2 rounded-lg hover:h-0" style={{backgroundColor:bbcolor}}> {bbcolor}</button>
-    </div>
+   
   )
 }
 
--- tproject04_bgChanger/src/App.jsx
+++ tproject04_bgChanger/src/App.jsx
@@ -7,8 +7,8 @@ const [color, setColor] = useState("grey");
   return (
     <>
      <div className = "w-full h-screen duration-200" style = {{backgroundColor: color}}>
-      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
-        <div className="bg-white gap-4 flex flex-wrap justify-center p-2 rounded-lg flex-row ">
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2" style={{backgroundColor:"RGBA(60,60,60,0.2)"}}>
+        <div className="gap-4 flex flex-wrap justify-center p-2 rounded-lg flex-row " style={{backgroundColor:"RGBA(211, 211, 211,0.5)"}}>
           <ButtonComp bbcolor="red" funck={() =>{setColor("red")}} ></ButtonComp>
 
           <ButtonComp bbcolor="green" funck={() =>{setColor("green")}} ></ButtonComp>
