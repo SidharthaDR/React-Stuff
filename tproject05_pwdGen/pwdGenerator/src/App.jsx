fef087d8dd2d35d12ac601eb780ee6e38a46fc2a
Sidharth_DR <raghunathsidhartha5h5@gmail.com>
2024-12-04 21:45:38 +0530

    update

--- tproject05_pwdGen/pwdGenerator/src/App.jsx
+++ tproject05_pwdGen/pwdGenerator/src/App.jsx
@@ -1,33 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useCallback, useState } from 'react'
 import './App.css'
+/*
+Notes:
+1. used "useCallback" for optimization -> It is a React Hook that lets you cache a function definition between re-renders.
+*/
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [length, setLength] = useState(8)
+  const [number, numberAllowed] = useState(false)
+  const [character, charAllowed] = useState(false)
+  const [password, setPassword] = useState("")
+
+  const passwordGenerator = useCallback(() => {
+    let pass = ""
+    let str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    if(number){
+      str+="123456789"
+    }
+    if(character){
+      str+="!@#$%^&*()_+.,;'[]"
+    }
+
+    for(let i = 0; i<length; i++){
+      pass +=  str.charAt(Math.floor((Math.random() * str.length)))
+    }
+    console.log("hello ",pass)
+  }, [length, number, character, setPassword])
+
+  passwordGenerator()
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="text-4xl text-center text-slate-200">Password Generator</h1>
+
+      <input type="text" id="" readOnly />
+
+      <input type="range" name="" id="" 
+      max={100}
+      min={8}
+      value={8}
+      />
     </>
   )
 }
