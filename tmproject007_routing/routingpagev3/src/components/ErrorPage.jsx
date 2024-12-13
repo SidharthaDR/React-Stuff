c6446781ce68a23935aca6a69e977149b58597c9
Sidharth_DR <raghunathsidhartha5h5@gmail.com>
2024-12-13 15:15:35 +0530

    update

--- tmproject007_routing/routingpagev3/src/components/ErrorPage.jsx
+++ tmproject007_routing/routingpagev3/src/components/ErrorPage.jsx
@@ -1,10 +1,12 @@
+import {useNavigate} from "react-router"
 function ErrorPage() {
-    // const navTo = useNavigate()
+    const navTo = useNavigate()
   return (
     <>
     <div>hello</div>
     <h1>Error 404: Page not found</h1>
-    {/* <button className='p-2 bg-black text-white'>Go Home</button> */}
+    <button className='p-2 bg-black text-white'
+    onClick={() => navTo('/')}>Go Home</button>
     </>
   )
 }
--- tmproject007_routing/routingpagev3/src/components/Footer.jsx
+++ tmproject007_routing/routingpagev3/src/components/Footer.jsx
@@ -1,7 +1,12 @@
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <>
+    <div 
+      className="bg-black text-white p-4 flex justify-center bottom-1 fixed right-0 left-0 mx-2 rounded-lg shadow-lg">
+      <h1>Footer</h1>
+      </div>
+    </>
   )
 }
 
