aba66aa9ac136118823bde8b8c9154ab1c7a0c99
Sidharth_DR <raghunathsidhartha5h5@gmail.com>
2024-12-15 07:16:06 +0530

    update

--- tmproject007_routing/routingpagev3/src/components/Home.jsx
+++ tmproject007_routing/routingpagev3/src/components/Home.jsx
@@ -1,7 +1,13 @@
 
 function Home() {
   return (
-    <div>Home</div>
+<div className="mx-auto w-full max-w-7xl">     
+   <h1>Home page</h1>
+      <img src="" alt="" />
+      
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. A ipsa natus sit labore facilis sequi modi quos, por</p>
+
+    </div>
   )
 }
 
--- tmproject007_routing/routingpagev3/src/layouts/LayoutOne.jsx
+++ tmproject007_routing/routingpagev3/src/layouts/LayoutOne.jsx
@@ -7,7 +7,8 @@ function LayoutOne() {
   return (
     <>
     <NavBar/>
-    <div className='flex justify-center items-end mt-10 text-[50px] font-mono'>
+    <div>
+    {/* <div className={'flex justify-center items-end mt-10 text-[50px] font-mono'}> */}
     <Outlet/>
     </div>
     <Footer/>
