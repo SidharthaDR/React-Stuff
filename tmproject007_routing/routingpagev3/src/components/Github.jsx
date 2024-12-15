6dcf09f7fee3e3f19ebbeb4a69a39e731848b83d
Sidharth_DR <raghunathsidhartha5h5@gmail.com>
2024-12-15 14:50:44 +0530

    update-working

--- tmproject007_routing/routingpagev3/src/components/Github.jsx
+++ tmproject007_routing/routingpagev3/src/components/Github.jsx
@@ -12,20 +12,25 @@ function Github() {
         fetch('https://api.github.com/search/users?q='+`${name}`, {
             headers: {Authorization: `Bearer ${my_token}`}
           })
-          .then((res) => res.json())
+          .then((res) => (
+            console.log("1st: ",res),
+              res.json()
+          )
+            )
           .then((data) => (
             console.log("yoo",data),
             data.items
           ))
           .then((data) => setData(data[0]))
-            
+          .catch((e) => console.error("user not found... ", e))
+            //sidharthads
     }, [name])
     /*
     Note: for fetching the data, you can use {} or () while using 
     .then(() => inThisPlace)
 
     the diffrence(s): 
-    1. you dont use "return" whle writing inside ()
+    1. you dont use "return" while writing inside (), last statement will be returned
     2. you use " , " to give next statement inside ()
     */
 
@@ -48,18 +53,34 @@ function Github() {
 
   return (
     <>
-        <div>
+        <div className='flex flex-col gap-5'>
+            
             <h1 className='text-center'>Github Page</h1>
+
+            <div className='flex flex-col items-center gap-3'>
+
             <p>results: {data.total_count}</p>
             {console.log("other:", data, "????")}
             <p>Login: {data.login}</p>
             <img src={data.avatar_url} width={300} alt="" />
 
+            </div>
 
-            <input type="text" name="" id="nextName" placeholder='search other user'/>
+            <div className='flex justify-center gap-3'>
+            <input type="text" name="" id="nextName" placeholder='search other user'
+            onKeyDown={(e) => {
+                if(e.key === "Enter"){
+                    setName(document.getElementById("nextName").value)
+                }
+            }
+                
+            }
+            />
             <button onClick={() => {
                 setName(document.getElementById("nextName").value)
             }}>Search</button>
+            </div>
+
         </div>
     </>
   )
